0250505913d648b58ab4fe3dd4547167470aef7f
SohelHak <155261518+SohelHak@users.noreply.github.com>
2024-04-04 05:11:09 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -23,3 +23,4 @@ echo """Type 'e-spam', to run tool
 
 Note: This email spam tool has been developed for educational purposes only. It is intended to demonstrate concepts related to email automation and should be used responsibly and ethically. Misuse of this tool for sending unsolicited or unauthorized emails is strictly prohibited and may violate laws and regulations related to email spamming and privacy. The developer assumes no liability for any misuse of this tool. Users are responsible for ensuring compliance with all applicable laws and regulations when using this tool. Use it responsibly and always respect the privacy and consent of others.
 """
+cd
